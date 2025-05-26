9485d53ca4558cb26adac1d612cef24317ec8e5a
salcedof <salcedomarcof91@gmail.com>
2025-05-26 15:53:42 -0600

    finished w3 individual3

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -8,7 +8,7 @@ renderList(list);{
 function productCardTemplate(product) {
   return `
     <div class="product-card">
-      <img src="${product.image}" alt="${product.name}">
+      <img src="${product.PrimaryMedium}" alt="${product.Name}">
       <h2>${product.name}</h2>
       <p>${product.description}</p>
       <span>$${product.price}</span>
